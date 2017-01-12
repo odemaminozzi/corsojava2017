e05fa64a4dfd73ac2dc53be41fa6c83d8aa95422
sviluppo <sviluppo@1F-PC03.STUDENTI.IT>
2017-01-12 10:09:14 +0100

    condizione

---
+++ snippets/animali/Condizione.java
@@ -0,0 +1,27 @@
+package esempi.week11;
+
+public class Condizione {
+
+	public static void main(String[] args) {
+		//if (1 == 0 || 1 ==1){//oppure 
+			
+			if (1 == 0 && 1 ==1){//and and
+			System.out.println("sono uguali");
+	
+			
+			
+			}else if(0 == 0){
+			
+				
+				System.out.println("sono uguali ma valgono zero 0");
+			
+			
+		}
+			
+			else{
+			System.out.println("sono diversi");
+		}
+
+	}
+
+}
